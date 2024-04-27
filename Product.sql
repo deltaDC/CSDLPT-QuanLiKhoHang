0b74ed07b83f2c71aaea8ad6ef942f333dca4631
Nguyen Thanh Long <129354101+long9703@users.noreply.github.com>
2024-04-27 19:44:57 +0700

    Add files via upload

--- Product.sql
+++ Product.sql
@@ -1,6 +1,6 @@
-INSERT INTO product (ID, product_name, price, category_id)
+﻿INSERT INTO product (ID, product_name, price, category_id)
 VALUES 
-	('P001' , N'�o c?c', 50000, 'CAT01'),
-	('P002' , N'�o d�i', 90000, 'CAT01'),
-	('P003' , N'Qu?n ?�i', 70000, 'CAT02'),
-	('P004' , N'Qu?n d�i', 110000, 'CAT02');
+	('P001' , N'Áo cộc', 50000, 'CAT01'),
+	('P002' , N'Áo dài', 90000, 'CAT01'),
+	('P003' , N'Quần đùi', 70000, 'CAT02'),
+	('P004' , N'Quần dài', 110000, 'CAT02');
