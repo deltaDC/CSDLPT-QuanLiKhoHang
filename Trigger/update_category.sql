00fb779c01ac2e249d55d3357c7475ed18b6a862
Nguyen Thanh Long <129354101+long9703@users.noreply.github.com>
2024-04-29 17:46:02 +0700

    Add files via upload

---
+++ Trigger/update_category.sql
@@ -0,0 +1,21 @@
+﻿CREATE TRIGGER update_category
+ON category
+AFTER INSERT,UPDATE
+AS
+BEGIN
+    -- Lấy ID từ bảng inserted
+    DECLARE @ID VARCHAR(50)
+    SET @ID = (SELECT TOP 1 ID FROM inserted)  -- Giả sử một hàng chèn
+
+    -- Kiểm tra ID có bắt đầu bằng 'CAT' không
+    IF @ID LIKE 'CAT%'
+    BEGIN
+        PRINT 'Chèn dữ liệu thành công.'
+    END
+    ELSE
+    BEGIN
+        -- Nếu không, hủy giao dịch và đưa ra thông báo lỗi
+        RAISERROR ('ID không hợp lệ, phải bắt đầu bằng "CAT".', 16, 1)
+        ROLLBACK TRANSACTION
+    END
+END
