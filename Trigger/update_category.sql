cb7cd3206166ab381e256262c5009b89e6c0d930
Nguyen Thanh Long <129354101+long9703@users.noreply.github.com>
2024-04-30 15:36:33 +0700

    update_category.sql

--- Trigger/update_category.sql
+++ Trigger/update_category.sql
@@ -1,5 +1,11 @@
-﻿CREATE TRIGGER update_category
-ON category
+USE [QuanLiKhoHang]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TRIGGER [dbo].[update_category]
+ON [dbo].[category]
 AFTER INSERT,UPDATE
 AS
 BEGIN
@@ -10,11 +16,12 @@ BEGIN
     -- Kiểm tra ID có bắt đầu bằng 'CAT' không
     IF @ID LIKE 'CAT%'
     BEGIN
-        PRINT 'Chèn dữ liệu thành công.'
+        PRINT 'Cập nhật dữ liệu thành công.'
     END
     ELSE
     BEGIN
         -- Nếu không, hủy giao dịch và đưa ra thông báo lỗi
+		PRINT 'Cập nhật thông tin thất bại.'
         RAISERROR ('ID không hợp lệ, phải bắt đầu bằng "CAT".', 16, 1)
         ROLLBACK TRANSACTION
     END
